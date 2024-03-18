ebee5817b093e2a6000173f07b06d5d0b659f031
salvatorecalo <salvatorecalo4@gmail.com>
2024-03-18 13:51:53 +0100

    feat: add laib01

--- find_maximum/main.c
+++ find_maximum/main.c
@@ -1,26 +1,5 @@
 #include <stdio.h>
 
-int	main(void)	{
-    // declare variables
-    int max, A, B;
-    // User enter values of A and B
-    printf("Enter the value of A: ");
-    scanf("%d", &A);
-    printf("Enter the value of B: ");
-    scanf("%d", &B);
-
-    // Check if A is greater than B
-    if	(A >= B){
-        // if A is greater than B, set the maximum to A
-        max	= A;
-    } else {
-        // if B is greater than A, set the maximum to B
-        max	= B;
-    }
-    // print the maximum
-    printf("The maximum is ");
-    printf("%d\n", max);
-
-
-    return 0;
+int	main(void) {
+    
 }
